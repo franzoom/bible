2cbc9248a88b502a2180bf8ddeda2835e9e1d5e1
François Labadens <124710681+franzoom@users.noreply.github.com>
2025-01-30 08:34:46 +0100

    ajouts

--- Ezechiel/assemblage.py
+++ Ezechiel/assemblage.py
@@ -1,18 +1,18 @@
 import fitz  # PyMuPDF
 
 # Ouvrir les documents PDF
-doc_a = fitz.open("Ez 33-37 TOB.pdf")
+doc_a = fitz.open("Cous sur Ezechiel.pdf")
 doc_b = fitz.open("Ez 33-37 Heb.pdf")
 
 # Créer un nouveau PDF pour le document final
 final_doc = fitz.open()
 
-# Ajouter les 3 premières pages du document A au document final
-for page_num in range(4):
+# Ajouter les 5 premières pages du document A au document final
+for page_num in range(6):
     final_doc.insert_pdf(doc_a, from_page=page_num, to_page=page_num)
 
 # Alterner les pages des documents A et B à partir de la page 4
-page_num_a = 4
+page_num_a = 6
 page_num_b = 0
 
 while page_num_a < len(doc_a) or page_num_b < len(doc_b):
